446585d0e7e0c5b48e646cb534e88ee343b93f42
Yeji <y22yoon@uwaterloo.ca>
2022-11-26 18:07:58 -0500

    commit

---
+++ PiceseType.h
@@ -0,0 +1,17 @@
+#ifndef _PIECESTYPE_H
+#define _PIECESTYPE_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Piece.h"
+using namespace std;
+
+class King: public Piece {
+
+
+public:
+
+};
+
+#endif
