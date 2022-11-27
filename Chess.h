d24e9840009d6b8b984e606984e32285ba3d6c0b
eDi9root <ojs3771o@gmail.com>
2022-11-26 19:41:06 -0500

    commit

--- Chess.h
+++ Chess.h
@@ -1,8 +1,18 @@
 #ifndef _CHESS_H
 #define _CHESS_H
+#include "Board.h"
+#include "Player.h"
 
 class Chess
 {
+    Board board;
+    Player player;
+
+public:
+    void resign();
+    void reset();
+    void turnmove();
+    void setup();
 };
 
 #endif
---
+++ Player.h
@@ -0,0 +1,14 @@
+#ifndef _PLAYER_H
+#define _PLAYER_H
+
+#include "Chess.h"
+
+class Player : public Chess
+{
+    std::string turn;
+
+public:
+    Move player();
+};
+
+#endif
