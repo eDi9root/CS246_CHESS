49757b27b66c77d6fd37133f97b15eac01e4d15b
eDi9root <ojs3771o@gmail.com>
2022-11-26 19:46:12 -0500

    commit

--- Board.h
+++ Board.h
@@ -7,12 +7,14 @@
 #include "Piece.h"
 using namespace std;
 
-class Board: public Piece{
-    //Tile **theBoard;
-    vector <Piece *> pieces;
+class Board : public Piece
+{
+    // Tile **theBoard;
+    vector<Piece *> pieces; // 이중 vector 이어야 할듯?
     string piecesType;
     string colour;
     string positions;
+
 public:
     Board();
 
