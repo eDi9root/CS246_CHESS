c46e08e358567dfd07fe57f8efd43246667dd6e4
eDi9root <119016575+eDi9root@users.noreply.github.com>
2022-11-28 16:18:16 -0500

    commit

--- Tile.h
+++ Tile.h
@@ -2,12 +2,23 @@
 #define _TILE_H
 
 #include "Board.h"
+#include "Piece.h"
+
+class Piece;
 
 class Tile {
+  int row;
+  int col;
+
  public:
+  Tile(int row, int col);
+  bool get_empty_tile();
+  void empty_tile();
   int getRow();
   int getCol();
   Piece* Piece();
 };
+
+std::ostream& operator<<(std::ostream& out, Tile& t);
 
 #endif
