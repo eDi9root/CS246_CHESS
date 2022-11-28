8a96ab9428a02c91674286c4553a7c9b244edb8e
eDi9root <119016575+eDi9root@users.noreply.github.com>
2022-11-27 23:34:18 -0500

    commit

--- Board.h
+++ Board.h
@@ -5,20 +5,26 @@
 #include <string>
 #include <vector>
 
+#include "Move.h"
 #include "Piece.h"
+#include "Tile.h"
 using namespace std;
 
 class Pieces;
 
-class Board : {
+class Move;
+
+class Board {
   // Tile **theBoard;
-  vector<vector<Piece *>> pieces;  // 이중 vector 이어야 할듯?
+  vector<vector<Piece*>> pieces;  // 이중 vector 이어야 할듯?
+  // Tile** theBoard;
   string piecesType;
   string colour;
   string positions;
 
  public:
   Board();
+  bool Move(Move move);
   void setup_placeP(string piecesType, string positions);
   // bool legal_movement(string pieceType, string x, string y);
 
