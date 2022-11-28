b6179ca6c8314697b7ef1dc848df225107168f90
eDi9root <119016575+eDi9root@users.noreply.github.com>
2022-11-28 00:17:50 -0500

    commit

--- Board.cc
+++ Board.cc
@@ -1,10 +1,16 @@
+#include "Board.h"
+
 #include <iostream>
 #include <vector>
-#include "Board.h"
+
+#include "PiecesType.h"
 #include "TextDisplay.h"
 using namespace std;
 
-
-void Board::setup_placeP(string piecesType, string x, string y, ) {
-
+Board::Board() {
+  // 0 으로 초기화
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+    }
+  }
 }
