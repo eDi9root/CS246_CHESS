30491ea2805d1462aea6b68a99387c9892a20f73
Yeji <y22yoon@uwaterloo.ca>
2022-11-28 19:33:39 -0500

    commit

--- main.cc
+++ main.cc
@@ -24,33 +24,39 @@ int main() {
     while (!cin.eof()) {
         cin >> command;
         if (command == "game") {
+            isongame = true;
             cin >> player1 >> player2;
+            /*
             if ((player1 == "white-player") || (player2 == "black-player")) {
-                Chess->player[0] = "white";
-                Chess->player[1] = "black"
             } else if (player1 == "human") {
                 if (player2 == "computer[1]") {
                     //...
                 }
             }
+            */
         } else if (command == "setup") { // consider about input "setup" first
-            setup_init();
-            cin >> setup;
-            while (true) {
-                if (setup == "+") {
-                    cin >> piecetype >> coordinate;
-                    if ((piecetype[0] == 'K' || piecetype[0] == 'Q' || piecetype[0] == 'B' || 
-                        piecetype[0] == 'R' || piecetype[0] == 'N' || piecetype[0] == 'P' ||
-                        piecetype[0] == 'k' || piecetype[0] == 'q' || piecetype[0] == 'b' || 
-                        piecetype[0] == 'r' || piecetype[0] == 'n' || piecetype[0] == 'p') &&
-                        ((coordinate[0] >= 'a' && coordinate[0] <= 'h') && 
-                        (coordinate[1] >= '1' && coordinate[1] <= '8'))) { // check if the command is valid
-                        setup_piceceP(piecetype[0], colour, coordinate[0], coordinate[1]); // from board
-                    } else {
-                        cout << "invalid setup!" << endl;
+            if (isongame == true) {
+                cout << "You cannot use setup mode while your plaqying game!" << endl;
+            } else {
+                cin >> setup;
+                while (true) {
+                    if (setup == "+") {
+                        cin >> piecetype >> coordinate;
+                        if ((piecetype[0] == 'K' || piecetype[0] == 'Q' || piecetype[0] == 'B' || 
+                            piecetype[0] == 'R' || piecetype[0] == 'N' || piecetype[0] == 'P' ||
+                            piecetype[0] == 'k' || piecetype[0] == 'q' || piecetype[0] == 'b' || 
+                            piecetype[0] == 'r' || piecetype[0] == 'n' || piecetype[0] == 'p') &&
+                            ((coordinate[0] >= 'a' && coordinate[0] <= 'h') && 
+                            (coordinate[1] >= '1' && coordinate[1] <= '8'))) { // check if the command is valid
+                            setup_piceceP(piecetype[0], colour, coordinate[0], coordinate[1]); // from board
+                        } else {
+                            cout << "invalid setup!" << endl;
+                        }
+                        /*
+                    } else if (command == "-") {
+                        //...
                     }
-                } else if (command == "-") {
-                    //...
+                    */
                 }
             }
 
@@ -71,7 +77,7 @@ int main() {
             }
             */
         } else if (setup == "redesign") {
-                
+                cout << "redesign" << endl;
         } else { // invalid command
             cout << "invalid command!" << endl;
         }
