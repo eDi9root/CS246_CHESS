302f4c35da453f10539dfb10ec7a2f9ae2ee1659
eDi9root <119016575+eDi9root@users.noreply.github.com>
2022-11-28 22:04:17 -0500

    commit

--- main.cc
+++ main.cc
@@ -6,8 +6,14 @@
 #include "Board.h"
 #include "Chess.h"
 #include "Move.h"
+#include "Observer.h"
+#include "Piece.h"
+#include "PiecesType.h"
 #include "Player.h"
+#include "Subject.h"
 #include "TextDisplay.h"
+#include "Tile.h"
+
 using namespace std;
 
 int main() {
@@ -32,11 +38,11 @@ int main() {
         if (player2 == "computer[1]") {
           //...
         }
-        
+
       }
       */
     } else if (command == "setup") {  // consider about input "setup" first
-      //setup_init();
+      // setup_init();
       cin >> setup;
       while (true) {
         if (setup == "+") {
@@ -48,9 +54,11 @@ int main() {
                piecetype[0] == 'b' || piecetype[0] == 'r' ||
                piecetype[0] == 'n' || piecetype[0] == 'p') &&
               ((coordinate[0] >= 'a' && coordinate[0] <= 'h') &&
-               (coordinate[1] >= '1' && coordinate[1] <= '8'))) {  // check if the command is valid
-              b->setup_placeP(piecetype[0], coordinate[0], coordinate[1]);  // from board
-              //notify(piecetype[0], coordinate[0],coordinate[1]);
+               (coordinate[1] >= '1' &&
+                coordinate[1] <= '8'))) {  // check if the command is valid
+            b->setup_placeP(piecetype[0], coordinate[0],
+                            coordinate[1]);  // from board
+            // notify(piecetype[0], coordinate[0],coordinate[1]);
           } else {
             cout << "invalid setup!" << endl;
           }
@@ -59,7 +67,7 @@ int main() {
           //...
         }
         */
-      }
+        }
       }
 
     } else if (command == "move") {
@@ -68,8 +76,8 @@ int main() {
           (newpos[0] >= 'a' && newpos[0] <= 'h') &&
           (originpos[1] >= '1' && originpos[1] <= '8') &&
           (newpos[1] >= '1' && newpos[1] <= '8')) {
-            cout << "move" << endl;
-        //move(originpos[0], originpos[1], newpos[0], newpos[1]);  // from ???
+        cout << "move" << endl;
+        // move(originpos[0], originpos[1], newpos[0], newpos[1]);  // from ???
       }
 
     } else if (command == "=") {  // makes the input's turn (i.e. white, black)
