18065f7645b62a67724336b94d338f46eb172329
eDi9root <119016575+eDi9root@users.noreply.github.com>
2022-11-28 21:13:08 -0500

    commit

--- PiecesType.cc
+++ PiecesType.cc
@@ -16,6 +16,8 @@ bool King::check_move(int ax, int ay, int bx, int by) const {
   return false;
 }
 
+Queen::Queen(std::string colour) : Piece(Piece::Queen, colour) {}
+
 bool Queen::check_move(int ax, int ay, int bx, int by) const {
   // 퀸은 모든 이동 가능 (나이트 제외)
   if (ax == bx) {
@@ -30,6 +32,18 @@ bool Queen::check_move(int ax, int ay, int bx, int by) const {
   return false;
 }
 
+Bishop::Bishop(std::string colour) : Piece(Piece::Bishop, colour) {}
+
+bool Bishop::check_move(int ax, int ay, int bx, int by) const {
+  // bishop은 대각선만
+  if (abs(ax - bx) == abs(ay - by)) {
+    return Piece::check_move(ax, ay, bx, by);
+  }
+  return false;
+}
+
+Rook::Rook(std::string colour) : Piece(Piece::Rook, colour) {}
+
 bool Rook::check_move(int ax, int ay, int bx, int by) const {
   // 룩은 직선만
   if (ax == bx) {
@@ -40,10 +54,45 @@ bool Rook::check_move(int ax, int ay, int bx, int by) const {
   return false;
 }
 
-bool Bishop::check_move(int ax, int ay, int bx, int by) const {
-  // bishop은 대각선만
-  if (abs(ax - bx) == abs(ay - by)) {
-    return Piece::check_move(ax, ay, bx, by);
+Knight::Knight(std::string colour) : Piece(Piece::Knight, colour) {}
+
+bool Knight::check_move(int ax, int ay, int bx, int by) const {
+  if (abs(1 - bx) == ax) {
+    if (abs(2 - by) == ay) {
+      return Piece::check_move(ax, ay, bx, by);
+    }
+  } else if (abs(2 - bx) == ax) {
+    if (abs(1 - by) == ay) {
+      return Piece::check_move(ax, ay, bx, by);
+    }
+  }
+  return false;
+}
+
+Pawn::Pawn(std::string colour) : Piece(Piece::Pawn, colour) {}
+
+bool Pawn::check_move(int ax, int ay, int bx, int by) const {
+  if (pcolour == "black") {
+    if (ax = bx && (ay > by && ay - 2 <= by)) {
+      return Piece::check_move(ax, ay, bx, by);
+    }
+  } else {
+    if (ax == bx && (ay < by && ay + 2 >= by)) {
+      return Piece::check_move(ax, ay, bx, by);
+    }
+  }
+  return false;
+}
+
+bool Pawn::check_attack(int ax, int ay, int bx, int by) const {
+  if (pcolour == "black") {
+    if ((abs(1 + ax) == bx) && (ay - 1 == by)) {
+      return true;
+    }
+  } else {
+    if ((abs(1 - ax) == bx) && (ay + 1 == by)) {
+      return true;
+    }
   }
   return false;
 }
