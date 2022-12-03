b129274178e175bc490f3dc5ec9ad724d5462e15
j36oh <j36oh@uwaterloo.ca>
2022-12-02 23:26:45 -0500

    c

---
+++ Human.cc
@@ -0,0 +1,3 @@
+#include "Human.h"
+
+#include "Piece.h"
---
+++ Human.h
@@ -0,0 +1,12 @@
+#ifndef HUMAN_H
+#define HUMAN_H
+
+#include <string>
+
+#include "Player.h"
+
+class Human : public Player {
+    Human(int turn);
+};
+
+#endif
